72425b174c8c818fb7f01ccd59bb6ccab5cf37a1
Ambrose Ling <lingambrose@gmail.com>
2024-07-24 12:05:16 -0400

    pivot index complete

--- find_pivot_index.py
+++ find_pivot_index.py
@@ -4,27 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # nums_dict = [0] * len(nums)
+        # visited = {}
         nums_dict = {}
         most_left_index = len(nums)-1
         left_sum = 0
         right_sum = 0
         for i in range(len(nums)):
             if i in nums_dict:
-                nums_dict[i] = nums_dict[i] + left_sum
+                nums_dict[i] += left_sum
             else:
                 nums_dict[i] = left_sum
-            if len(nums)-i-1 in nums_dict:
-                nums_dict[len(nums)-i-1] = nums_dict[len(nums)-i-1] -right_sum
+
+            if len(nums)-1-i in nums_dict:
+                nums_dict[len(nums)-i-1] -= right_sum
             else:
-                nums_dict[len(nums)-i-1] = right_sum
+                nums_dict[len(nums)-i-1] = -1*right_sum
+
             left_sum += nums[i]
             right_sum += nums[len(nums)-i-1]
-            if i != 0 or i != len(nums)-1:
-                if nums_dict[i] == 0:
+            if i != 0 and i != len(nums)-1 and i >= len(nums)//2 and len(nums)-i-1 <= len(nums)//2:
+                if i in nums_dict and nums_dict[i] == 0:
                     if i < most_left_index:
                         most_left_index =i
-                if nums_dict[len(nums)-i-1] == 0:
+                if len(nums)-i-1 in nums_dict and nums_dict[len(nums)-i-1] == 0:
                     if len(nums)-i-1 < most_left_index:
                         most_left_index = len(nums)-i-1
-        print(nums_dict)
+            
+        if nums_dict[0] == 0:
+            return 0
+        if most_left_index == len(nums)-1:
+            if nums_dict[len(nums)-1] == 0:
+                return most_left_index
+            else:
+                return -1
         return most_left_index 
